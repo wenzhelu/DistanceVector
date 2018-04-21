6c98e1cd068ef00f1262df043a1dfb33d468e0e3
yunhli <yunhaoli@DESKTOP-0ECE5QP.localdomain>
2018-04-21 17:36:56 -0400

    config file

--- DV.cpp
+++ DV.cpp
@@ -11,6 +11,39 @@
 
 DV::DV(string config, string port, string poison, string TTL, string prd) {
     // init routing table from config and port
+	FILE *fp = fopen(config, O_RDONLY);
+	char addr[20];
+	char cost[4];
+	if (fp == NULL)
+	{
+		printf("config file not found\n");
+		exit(1);
+	}
+	//for each line, addr is the address and cost 
+	while (fscanf(fp, "%s %d\n", addr, cost) != EOF)
+	{
+
+		uint dest = inet_addr(addr);
+		RouterTableEntry line;
+
+		line.ttl = TTL;
+		if (cost[0] = 'y')
+		{
+			//set value for rTable
+			line.cost = 1;
+			line.next = inet_addr(addr);
+			//set value for remoteIP
+			usock->remotes.push(addr);
+		}
+		else
+		{
+			line.cost = infin;
+			line.next = 0;        // 0 for NULL next router
+		}
+		rTable.insert({ dest,line });
+		memset(addr, 0, 20);
+		memset(cost, 0, 4);
+	}
 }
 
 // constructors for DV
--- main.cpp
+++ main.cpp
@@ -8,18 +8,40 @@
 
 int main(int argc, const char * argv[]) {
     
-    if (argc < 4) {
-        cout << "usage: " << endl;
-        cout << "./dv config_file port# poison_reverse(true/false) [TTL] [timer_slot]" << endl;
-    }
-    
-    DV dv;
-    if (argc > 4) {
-        dv.ttl = stoi(argv[4]);
-        if (argc > 5) dv.period = stoi(argv[5]);
-    }
-    
-    dv.init(argv[1], argv[2], argv[3]);
-    
-    return 0;
+	int argCount = 7;
+
+	char config[40];  //length of file name should be less than 40 bytes
+	FILE * fp;
+	int portNumber;
+	int TTL;
+	int inf;
+	int period;   // period seconds
+	bool reverse; // input should  be true or false
+
+	if (argc != argCount)
+	{
+		printf("Usage:config, portnumber, TTL, infinity, Period, Poison Reverse\n");
+		exit(1);
+	}
+	else
+	{
+		// initial parameters
+		int index = 0;
+		while (argv[1][index] != '\0')
+		{
+			config[index] = argv[1][index];
+			index++;
+		}
+		config[index] = '\0';
+		fp = fopen(config, O_RDONLY);
+
+		portNumber = atoi(argv[2]);
+		TTL = atoi(argv[3]);
+		inf = atoi(argv[4]);
+		period = atoi(argv[5]);
+		reverse = *argv[6] == 't' ? true : false;
+
+		//call some functions
+	}
+	return 0;
 }
