53b423c0683e67061f9c738e641bb4c41196a49a
wenzhelu <wenzhe.lu92@gmail.com>
2018-04-22 17:15:16 -0400

    bugfix#6

--- UDPSock.cpp
+++ UDPSock.cpp
@@ -41,7 +41,7 @@ int UDPSock::init() {
 // send routing table information to neighbors
 void UDPSock::write() {
     for (auto& remote : remotes) {
-        cout << remote.
+        cout << "Sending to IP: " << DV::uintToIP(remote.sin_addr.s_addr) << endl;
         size_t written = sendto(socket_fd, dv->sendBuff, dv->buffLen, 0, (struct sockaddr *)&remote, sizeof(remote));
         if (written != dv->buffLen) {
             cout << "write socket error" << endl;
